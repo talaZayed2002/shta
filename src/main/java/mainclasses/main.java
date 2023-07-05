fc91f2874503457670430a792298b6ce7bd7691c
shafiqaabdat <shafiqaabdat@air-shafiqa>
2023-07-05 03:55:27 +0300

    main :(

--- src/main/java/mainclasses/main.java
+++ src/main/java/mainclasses/main.java
@@ -7,21 +7,52 @@ import java.util.logging.Logger;
 public class main {
 private static final Logger logger = Logger.getLogger(main.class.getName());
 static Scanner s=new Scanner(System.in);
+static int num ;
+static String email="0";
+static String password="0";
 static void start1() {
 	  logger.info(" \n"+"                                    HELLO\n"+
-	  		      "                            WELCOME TO SAKANCCOM \n"+
+	  		      "                            WELCOME TO SAKANCOM \n"+
                   "                    Please choose your type: \n" +
 			      "                    1-Admin \n"+
                   "                    2-Tenant\n"+
 			      "                    3-Owner\n") ;
-	 
+		
+	    num=s.nextInt();
+		switch(num)
+		{
+		case 1:
+		{
+			 logger.info("Please enter your email:"); 
+				email=s.nextLine();
+				email=s.nextLine();
+				while(!(email.contains("@"))) {
+					logger.info("Please this is not an invalid email\n reenter ur email"); 
+					email=s.nextLine();
+				}
+		     logger.info("\nPlease enter your password:"); 
+		      	password=s.nextLine();
+		 
+			break;
+		}
+	
+		default :
+		{
+			logger.info("The enteryis not correct!");
+		}
+		
+			
+		
+		}//end switch
+		
 }
 
 
 public static void main(String[] args) 
 {	
 	start1();
-	
+
+		
 }//end public static void main(String[] args) 
 
 
